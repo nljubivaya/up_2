4390450eb9c0ba06b81cea84c8bd97e09f04d2f1
nljubivaya <nastyshalubivaya@gmail.com>
2025-03-15 10:32:41 +0300

    +tests

--- ClassLibrary1/ClassLibrary1/obj/Debug/net8.0/SF2022User{NN}Lib.AssemblyInfo.cs
+++ ClassLibrary1/ClassLibrary1/obj/Debug/net8.0/SF2022User{NN}Lib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SF2022User{NN}Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c6440b789f6c4fa07c7e21dfa19d478cf22c11")]
 [assembly: System.Reflection.AssemblyProductAttribute("SF2022User{NN}Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SF2022User{NN}Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UP2/UP2/obj/Debug/net8.0/WriteAndErase.AssemblyInfo.cs
+++ UP2/UP2/obj/Debug/net8.0/WriteAndErase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WriteAndErase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49fffc8f77ed8d077aa21531913f98f2f638262c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c6440b789f6c4fa07c7e21dfa19d478cf22c11")]
 [assembly: System.Reflection.AssemblyProductAttribute("WriteAndErase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WriteAndErase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
